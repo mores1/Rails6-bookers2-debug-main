9cdcf2932256782279afc72d5edc4164a78678d0
EC2 Default User <ec2-user@ip-172-31-39-113.ec2.internal>
2022-03-02 19:21:38 +0000

    [220303my8b]

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,22 +1,27 @@
 class Book < ApplicationRecord
-  
+
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   validates :title, presence: true
   validates :body, presence: true, length:{maximum:200}
-  
+
   is_impressionable
-  
+
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
-  
+
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+
+  scope :created_sixdayago, -> { where(created_at: 6.day.ago.all_day) }
+  scope :created_fivedayago, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_fourdayago, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_threedayago, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_twodayago, -> { where(created_at: 2.day.ago.all_day) }
   
-  
-  
+
 
   def self.looks(search, word)
     if search == "perfect_match"
